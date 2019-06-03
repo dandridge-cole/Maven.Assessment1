07cc1ce507c8d96008621d6d95088bec86967842
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-03 12:39:46 -0400

    PART 4 DONE

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -5,7 +5,5 @@ public class Jumper {
     /*
      * Complete the function below.
      */
-    public int jumps(int k, int j) {
-        return -1;
-    }
+    public int jumps(int k, int j) { return k/j+k%j;}
 }
