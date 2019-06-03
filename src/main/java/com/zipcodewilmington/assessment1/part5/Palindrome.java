170eea062e406942717ce1c036f434d3c022a263
Abram Phillips <abramcpg@hotmail.com>
2019-06-03 16:23:01 -0400

    41 tests passed

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,8 +3,16 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-    Integer s = 0;
+    int length = input.length();
+    Integer answer = length;
+    for (int i = 1; i <= length; i++){
+        for (int j = i - 1, k = i; j >= 0 && k < length; j--, k++) {
+            if (input.charAt(j) == input.charAt(k)) {
+                answer++;
+            } else break;
+        }
 
-        return s;
+    }
+        return answer;
     }
 }
